b851775ffa9aa2eb83c6d6a50f778f4defdfd785
rmontero2021 <rmontero2021@hotmail.com>
2024-01-15 13:24:51 -0400

    the initial project configuration was added

---
+++ TaskManager/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Task> Courses { get; set; }
+    }
+}
---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string? Descripcion { get; set; }
+        
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime? FechaCreacion { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string? Estado { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Prioridad { get; set; }
+    }
+}
